2fcbc719c13760edc76ec2cab23164388a515e94
franklinetush1 <franklinetush1@gmail.com>
2023-09-27 06:59:45 -0700

    pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for _ in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
+
+        for i in range(1, len(prev_row)):
+            new_row.append(prev_row[i - 1] + prev_row[i])
+
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
