b33136ba127444a73203a2263bd43c394c18e03f
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-10-12 12:41:44 +0300

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,20 +3,22 @@
 
 
 def minOperations(n):
-  """Determines minimum operations"""
-    if n < 2:
-        return n  # If n is less than 2, no Copy All is needed.
-
-    operations = 0  # Initialize the count of operations.
-    clipboard = 0  # Initialize the clipboard.
-
-    while n > 1:
-        if n % 2 == 0:
-            clipboard = n // 2
-        else:
-            clipboard = n
-
-        n -= clipboard
-        operations += 1
-
-    return operations
+    """
+    Gets fewest # of operations needed to result in exactly n H characters
+    """
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
+        return 0
+    ops, root = 0, 2
+    while root <= n:
+        # if n evenly divides by root
+        if n % root == 0:
+            # total even-divisions by root = total operations
+            ops += root
+            # set n to the remainder
+            n = n / root
+            # reduce root to find remaining smaller vals that evenly-divide n
+            root -= 1
+        # increment root until it evenly-divides n
+        root += 1
+    return ops
