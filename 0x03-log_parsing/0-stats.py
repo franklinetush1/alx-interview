07854ead671a7c7aa4bdf1ce316b2c02065e6105
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-10-19 21:10:20 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,22 +2,41 @@
 import sys
 import signal
 
+# Initialize variables to keep track of metrics
 total_file_size = 0
 status_code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
 
-def print_statistics():
-    print("File size:", total_file_size)
-    for code in sorted(status_code_count.keys()):
-        if status_code_count[code] > 0:
-            print(f"{code}: {status_code_count[code]}")
+# Function to print statistics
+def print_msg(dict_sc, total_file_size):
+    """
+    Method to print the file size and status code counts.
 
+    Args:
+        dict_sc (dict): A dictionary with status codes as keys and their counts as values.
+        total_file_size (int): The total file size.
+    """
+    print("File size:", total_file_size)
+    for code in sorted(dict_sc.keys()):
+        if dict_sc[code] > 0:
+            print(f"{code}: {dict_sc[code]}")
+
+# Function to handle Ctrl+C
 def signal_handler(signal, frame):
-    print_statistics()
+    """
+    Signal handler for Ctrl+C. It prints the statistics and exits.
+
+    Args:
+        signal: The signal being handled.
+        frame: The current stack frame.
+    """
+    print_msg(status_code_count, total_file_size)
     sys.exit(0)
 
+# Register the Ctrl+C signal handler
 signal.signal(signal.SIGINT, signal_handler)
 
+# Process input line by line
 for line in sys.stdin:
     try:
         parts = line.strip().split()
@@ -34,7 +53,8 @@ for line in sys.stdin:
 
         # Print statistics every 10 lines
         if line_count % 10 == 0:
-            print_statistics()
+            print_msg(status_code_count, total_file_size)
 
     except (ValueError, IndexError):
+        # Ignore lines that don't match the expected format
         continue
