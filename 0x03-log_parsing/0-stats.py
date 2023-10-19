dec010ed83dccecf376568007bdc31b48d4eeb1a
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-10-19 21:11:13 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,60 +1,54 @@
 #!/usr/bin/python3
+
 import sys
-import signal
 
-# Initialize variables to keep track of metrics
-total_file_size = 0
-status_code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
 
-# Function to print statistics
 def print_msg(dict_sc, total_file_size):
     """
-    Method to print the file size and status code counts.
-
+    Method to print
     Args:
-        dict_sc (dict): A dictionary with status codes as keys and their counts as values.
-        total_file_size (int): The total file size.
+        dict_sc: dict of status codes
+        total_file_size: total of the file
+    Returns:
+        Nothing
     """
-    print("File size:", total_file_size)
-    for code in sorted(dict_sc.keys()):
-        if dict_sc[code] > 0:
-            print(f"{code}: {dict_sc[code]}")
 
-# Function to handle Ctrl+C
-def signal_handler(signal, frame):
-    """
-    Signal handler for Ctrl+C. It prints the statistics and exits.
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
-    Args:
-        signal: The signal being handled.
-        frame: The current stack frame.
-    """
-    print_msg(status_code_count, total_file_size)
-    sys.exit(0)
 
-# Register the Ctrl+C signal handler
-signal.signal(signal.SIGINT, signal_handler)
+total_file_size = 0
+code = 0
+counter = 0
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
 
-# Process input line by line
-for line in sys.stdin:
-    try:
-        parts = line.strip().split()
-        if len(parts) != 7:
-            continue
-        ip, _, _, status_code, file_size = parts[0], parts[5], parts[6]
-        status_code = int(status_code)
-        file_size = int(file_size)
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
 
-        # Update metrics
-        total_file_size += file_size
-        status_code_count[status_code] += 1
-        line_count += 1
+        if len(parsed_line) > 2:
+            counter += 1
 
-        # Print statistics every 10 lines
-        if line_count % 10 == 0:
-            print_msg(status_code_count, total_file_size)
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
 
-    except (ValueError, IndexError):
-        # Ignore lines that don't match the expected format
-        continue
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
+
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
+                counter = 0
+
+finally:
+    print_msg(dict_sc, total_file_size)
