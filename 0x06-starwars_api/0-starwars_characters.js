1451be7907f3d17b951bbcbd8949e0ef0ae9f102
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-11-09 23:07:01 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,18 +1,26 @@
 #!/usr/bin/node
+const argv = process.argv;
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/?format=json';
-request.get(url, function (response, body) {
-  const film = (JSON.parse(body.body));
-  // debugging
-  // console.log(film);
-  // console.log(film.characters);
-  character(film.characters);
-});
-const character = (people, i = 0) => {
-  if (i === people.length) return;
-  request.get(people[i], function (response, body) {
-    // console.log(people[i]);
-    console.log(JSON.parse(body.body).name);
-    character(people, i + 1);
+
+function getPromise (url) {
+  const promise = new Promise(function (resolve, reject) {
+    request(url, function (err, res, body) {
+      if (err) reject(err);
+      else resolve(JSON.parse(body).name);
+    });
   });
-};
+  return promise;
+}
+
+request(
+  `https://swapi-api.hbtn.io/api/films/${argv[2]}`,
+  async function (err, res, body) {
+    if (err) throw err;
+    const data = JSON.parse(body).characters;
+
+    for (const i of data) {
+      const result = await getPromise(i);
+      console.log(result);
+    }
+  }
+);
